244e617790d4be0570301cbd9f6fb6eba784862a
Jimson Paul Serafin <paul.serafin@hcdc.edu.ph>
2022-10-04 10:58:01 +0800

    Initial Save of the project

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿class Names
+{
+    
+    public void nums(){
+        int[] num = {10,20,30};
+        for(int i = 0; i < num.Length; i++){
+            Console.WriteLine(num[i]);
+        }
+    }
+
+    String[] names = {"Jimson", "Paul", "Serafin"};
+
+    static void Main (String[] args){
+        Names name = new Names();
+        
+        for(int i = 0; i < name.names.Length; i ++){
+            Console.WriteLine(name.names[i]);
+        }
+        name.nums();
+
+        Console.WriteLine("Hello! What's your name? ");
+        string s = Console.ReadLine();
+        Console.WriteLine("Hello!\t"+ s);
+        Console.ReadKey(); 
+    }
+
+}
