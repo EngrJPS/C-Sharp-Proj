f6740112cf4335691a66a09f17f398171db3debd
Jimson Paul Serafin <paul.serafin@hcdc.edu.ph>
2022-10-16 23:15:42 +0800

    Setups the activity1 file

--- Activity1/Program.cs
+++ Activity1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace activity1{
+    class activity1{
+        public static void Main(string[] args){
+            Console.WriteLine("Activity1");
+        }
+    }
+}
