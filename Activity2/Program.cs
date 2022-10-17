9a982826dd71bbb78840569b12f4445b18f78990
Jimson Paul Serafin <paul.serafin@hcdc.edu.ph>
2022-10-17 23:40:27 +0800

    Partial functions added to the activity

--- Activity2/Program.cs
+++ Activity2/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace activity2{
+    /*
+    Make a program that uses List
+    Make an interactive program that Lists all the item
+    */
     class Activity2{
         public static void Main(string[] args){
 
@@ -30,6 +35,47 @@ namespace activity2{
             
             Console.WriteLine(" ");
             Console.WriteLine(result/arr.Length);
+
+            Console.Write("Please enter a number for the weight of the array:\t");
+            int data = Convert.ToInt32(Console.ReadLine() ?? "0");
+
+            arrNames(data);
+        }
+
+        public static void arrNames(int data){
+            string[] input = new string [data];
+
+            List<string> something = new List<string>(input);
+
+            Console.WriteLine("Write names:\t");
+            for(int i = 0; i < input.Length; i++){
+                string name = Console.ReadLine() ?? "0";
+                input[i] = name;
+                something.Add(input[i]);
+            }
+            
+            foreach(string somethings in something){
+                Console.Write(somethings + " ");
+            }
+
+            Console.WriteLine($"\n Capacity of input: \t{something.Capacity}\n");
+            something.AddRange(input);
+
+            Console.WriteLine("Write another names:");
+
+            input = new string[data];
+            
+            for(int i = 0; i < input.Length; i++){
+                string name = Console.ReadLine() ?? "0";
+                input[i] = name;
+                something.Add(input[i]);
+            }
+
+            // something.InsertRange(input.Length, input);
+
+            foreach(string somethings in something){
+                Console.Write(somethings + " ");
+            }
         }
     }
 }
