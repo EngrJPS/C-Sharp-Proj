cc175d4979f5d579e264de6a52f398972b0027ae
Jimson Paul Serafin <paul.serafin@hcdc.edu.ph>
2022-10-17 19:45:36 +0800

    Initial code for activity2

--- Activity2/Program.cs
+++ Activity2/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace activity2{
+    class Activity2{
+        public static void Main(string[] args){
+
+            int result = 0;
+
+            Console.Title = "Activity2";
+            Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+
+            int []arr = new int[4];
+
+            for(int i = 0; i < arr.Length; i++){
+                
+                int numRandom = rnd.Next(1, 10);
+                arr[i] = numRandom;
+                Console.Write(arr[i] + " ");
+                string sNum = arr[i].ToString();
+                int iNum = Convert.ToInt32(sNum);
+                result += iNum;
+            }
+
+            Console.WriteLine(" ");
+
+            foreach(int i in arr){
+                Console.Write(i + " ");
+            }
+            
+            Console.WriteLine(" ");
+            Console.WriteLine(result/arr.Length);
+        }
+    }
+}
