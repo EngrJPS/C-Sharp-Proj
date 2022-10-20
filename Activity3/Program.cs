1e2987bc4bc7d63ddac9decde701e746fa6a18ce
Jimson Paul Serafin <paul.serafin@hcdc.edu.ph>
2022-10-20 15:27:02 +0800

    Polished some of the functions in the activity.

--- Activity3/Program.cs
+++ Activity3/Program.cs
@@ -4,16 +4,23 @@ namespace dinnerDate{
     class dinnerDate{
         public static void Main(string[] args){
             var lasanga = new Lasanga();
-            Lasanga lasanga1 = new Lasanga();
 
             Console.Write("The expected minutes to cook a lasanga is:\t" + lasanga.ExpectedMinutesOven()+"\n");
+
             Console.Write("How long is the lasanga in the oven?\t");
-            int timeLeft = Convert.ToInt32(Console.ReadLine() ?? "0");
-            Console.WriteLine("Time remaining to cook the lasanga is\t"+ lasanga.RemainingMinutesInOver(timeLeft));
-            Console.Write("How many layers to of lasanga you will prepare?\t");
-            int prepTime = Int32.Parse(Console.ReadLine()?? "0");
-            Console.WriteLine($"The time to prep a {prepTime} layer/s is " + lasanga.PreparationTime(prepTime)+ " mins");
-            Console.Write("The lasanga needs to be in the oven for " + lasanga.ElapsedTimeInMinutes(prepTime, timeLeft)+ " mins");
+            int timeOven = Convert.ToInt32(Console.ReadLine() ?? "0");
+            Console.WriteLine("Time remaining to cook the lasanga is:\t"+ lasanga.RemainingMinutesInOver(timeOven));
+
+            Console.Write("How many layers to of lasanga did you prepare?\t");
+            int layerLasanga = Int32.Parse(Console.ReadLine()?? "0");
+
+            if(layerLasanga == 1){
+                Console.WriteLine($"The time to prep a {layerLasanga} layer of lasanga is " + lasanga.PreparationTimeInMinutes(layerLasanga)+ " mins");
+            }else{
+                Console.WriteLine($"The time to prep a {layerLasanga} layers of lasanga are " + lasanga.PreparationTimeInMinutes(layerLasanga)+ " mins");
+            }
+
+            Console.Write("The total time you've worked on the lasanga is " + lasanga.ElapsedTimeInMinutes(layerLasanga, timeOven)+ " mins");
         }
     }
 
@@ -21,7 +28,7 @@ namespace dinnerDate{
 
         int mins = 40;
 
-        int totalPrep = 0;
+        int totalLayer = 0;
 
         public int ExpectedMinutesOven(){
             return mins;
@@ -30,13 +37,13 @@ namespace dinnerDate{
             return mins - value;
         }
         
-        public int PreparationTime(int prep){
-            totalPrep = prep * 2;
-            return totalPrep;
+        public int PreparationTimeInMinutes(int layerLasanga){
+            totalLayer = layerLasanga * 2;
+            return totalLayer;
         }
 
         public int ElapsedTimeInMinutes(int layers, int numMinOven){
-            return (layers * 2) + numMinOven;
+            return PreparationTimeInMinutes(layers) + numMinOven;
         }
     }
 }
