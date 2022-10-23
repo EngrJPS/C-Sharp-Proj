5f3aeccc2a54edd85834e06113d851e731a30ba7
Jimson Paul Serafin <paul.serafin@hcdc.edu.ph>
2022-10-23 14:44:06 +0800

    Intial Function for Activity6

---
+++ Activity6/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Activity6{
+    class Activity6{
+        public static void Main(string[] args){
+            var spm = new SecurityPassMaker();
+            Console.WriteLine(spm.GetDisplayName(new Manager()));
+            Console.WriteLine(spm.GetDisplayName(new Physio()));
+        }
+    }
+
+    public class SecurityPassMaker{
+
+        public string GetDisplayName(TeamSupport support){
+            if(support is Staff){
+                var displayName = support.Title;
+                if(!(support is Manager)){
+                    displayName += " Priority Personnel";
+                }
+                return displayName;
+            }
+            return "Too Important for a Security Pass";
+        }
+    }
+        public interface TeamSupport{
+            string Title{
+                get;
+            }
+        }
+        public abstract class Staff : TeamSupport{
+            public abstract string Title{
+                get;
+            }
+        }
+
+        public class Manager : TeamSupport{
+            public string Title{
+                get;
+            }
+            = "The Manager";
+        }
+
+        public class Chairperson : TeamSupport{
+            public string Title{
+                get;
+            } = "The Chairperson";
+        }
+
+        public class Physio : Staff{
+            public override string Title{
+                get;
+            }= "The Physio";
+        }
+
+        public class JuniorSecurity : Staff{
+            public override string Title{
+                get;
+            } = "Security Junior";
+        }
+}
