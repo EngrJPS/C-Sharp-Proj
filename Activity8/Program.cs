7fe2265a6899dfe3f630b923f66197de0596bf94
Jimson Paul Serafin <paul.serafin@hcdc.edu.ph>
2022-11-06 21:32:31 +0800

    Initial functions for the Activity 8

--- Activity8/Program.cs
+++ Activity8/Program.cs
@@ -6,23 +6,33 @@ namespace Activity8{
         public static void Main(string[] args){
             string name = "my   ID";
             Identifier.Clean(name);
-            Identifier.Clean("my/0ID");
+            Identifier.Clean("my\0ID");
+            Identifier.Clean("a-ḃc");
         }
     }
 
     class Identifier{
         
         public static void Clean(string notClean){
-            char[] special = {'/','\'', '0', '1'};
-            char []charArr = new char [notClean.Length];
-            StringBuilder sb = new StringBuilder(notClean, 50);
-            sb.Replace(' ','_');
-            for(int i = 0; i < notClean.Length; i++){
-                charArr[i] = notClean[i];
-                for(int j = 0; j < special.Length; j++){
-                    if(charArr[i] == special[j]){
-                        sb.Replace(special[j], ' ');
+            StringBuilder sb = new StringBuilder(notClean, notClean.Length);
+            foreach(char character in notClean){
+                if(character == ' '){
+                    sb.Replace(' ','_');
+                }else{
+                    if(char.IsControl(character)){
+                        sb.Append("CTRL");
+                        continue;
                     }
+                    if(character == '-'){
+                        if(character == 'ḃ'){
+                            sb.Append('Ḃ');
+                        }
+                    }
+
+                }
+
+                if(char.IsControl(character)){
+                    
                 }
             }
             Console.WriteLine(sb.ToString());
