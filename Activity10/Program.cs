fdd4862dae29018b1aaa148b99dcc63974c2f61d
Jimson Paul Serafin <paul.serafin@hcdc.edu.ph>
2022-11-10 17:02:06 +0800

    Added the Activity10 called Anagram

---
+++ Activity10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Activity10{
+    class Activity10{
+        public static void Main(string[] args){
+            stringAnagram.Anagram("Stone");
+        }
+    }
+
+    class stringAnagram{
+        public static void Anagram(string ACharacters){
+            string[] choices = new string[]{"Stone", "tones", "tons", "notes", "Seton"};
+            foreach(string anagrams in choices){
+                switch(ACharacters){
+                    case "Stone":
+                    Console.Write(anagrams + " ");
+                    break;
+                }
+            }
+        }
+    }
+}
