ce1c674c3d6a523fc89ff226ecd8a21f9dd34e0c
EngrJPS <paul.serafin@hcdc.edu.ph>
2022-11-21 22:29:05 +0800

    Pushed and finished Activity12

---
+++ Activity12/Program.cs
@@ -0,0 +1,76 @@
+﻿//Exercism Exercise called 'Bob'
+
+/*
+Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+
+Bob answers 'Sure.' if you ask him a question, such as "How are you?".
+
+He answers 'Whoa, chill out!' if you YELL AT HIM (in all capitals).
+
+He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
+
+He says 'Fine. Be that way!' if you address him without actually saying anything.
+
+He answers 'Whatever.' to anything else.
+
+Bob's conversational partner is a purist when it comes to written communication and always follows normal rules regarding sentence punctuation in English.
+*/
+
+using System;
+using System.Timers;
+
+namespace Activity12{
+    class Activity12{
+        
+        public static void Main(String[] args){
+            Console.WriteLine("You can now interact with Bob!");
+            bool dec = true;
+            while(dec){
+                Console.WriteLine(lackadaisicalBob.Response(Console.ReadLine()??""));
+                Console.Write("Do you want to interact again? ");
+                string choice = Console.ReadLine()??" ";
+
+                switch(choice){
+                    case "Y":
+                    Console.WriteLine(lackadaisicalBob.Response(Console.ReadLine()??""));
+                    break;
+                    case "N":
+                    dec = false;
+                    break;
+                    default:
+                    Console.WriteLine("I don't know anymore");
+                    break;
+                }
+                Console.WriteLine("Thank you for using this app!");
+            }
+        }
+        
+    }
+
+    class lackadaisicalBob{
+
+        
+        public static string Response(string message){
+
+            if(isSilence(message))
+                return "Fine. Be that way!";
+            if(isQuestion(message) && isYell(message))
+                return "Calm down, I know what I'm doing!";
+            if(isYell(message))
+                return "Whoa, chill out!";
+            return "Whatever";
+        }
+
+        private static bool isSilence(string message){
+            return string.IsNullOrWhiteSpace(message);
+        }
+
+        private static bool isQuestion(string message){
+            return message.TrimEnd().EndsWith("?");
+        }
+
+        private static bool isYell(string message){
+            return message.Any(char.IsLetter) && message.ToUpperInvariant() == message;
+        }
+    }
+}
