e4a07d1b9b0b5cac1c20f92dc8e9276e845a7b70
EngrJPS <paul.serafin@hcdc.edu.ph>
2022-11-27 23:16:14 +0800

    Learned substring for today @ Activity 13/

--- Activity12/Program.cs
+++ Activity12/Program.cs
@@ -20,44 +20,52 @@ using System;
 using System.Timers;
 
 namespace Activity12{
+    
     class Activity12{
         
         public static void Main(String[] args){
             Console.WriteLine("You can now interact with Bob!");
             bool dec = true;
-            while(dec){
-                Console.WriteLine(lackadaisicalBob.Response(Console.ReadLine()??""));
-                Console.Write("Do you want to interact again? ");
-                string choice = Console.ReadLine()??" ";
-
-                switch(choice){
-                    case "Y":
+            lackadaisicalBob.intervalTimer();
+            string ans = Console.ReadLine()??" ";
+            if(!string.IsNullOrWhiteSpace(ans)){
+                while(dec){
                     Console.WriteLine(lackadaisicalBob.Response(Console.ReadLine()??""));
-                    break;
-                    case "N":
-                    dec = false;
-                    break;
-                    default:
-                    Console.WriteLine("I don't know anymore");
-                    break;
+                    Console.Write("Do you want to interact again? ");
+                    string choice = Console.ReadLine()??" ";
+
+                    switch(choice){
+                        case "Y":
+                        Console.WriteLine(lackadaisicalBob.Response(Console.ReadLine()??""));
+                        break;
+                        case "N":
+                        dec = false;
+                        break;
+                        default:
+                        Console.WriteLine("I don't know anymore");
+                        break;
+                    }
+                    Console.WriteLine("Thank you for using this app!");
                 }
-                Console.WriteLine("Thank you for using this app!");
             }
+            lackadaisicalBob.tTimes.Stop();
+            lackadaisicalBob.tTimes.Dispose();
+            Console.WriteLine("Thank you for using this app!");
         }
         
     }
 
     class lackadaisicalBob{
-
-        
+        public static System.Timers.Timer tTimes;
         public static string Response(string message){
-
+            
             if(isSilence(message))
                 return "Fine. Be that way!";
             if(isQuestion(message) && isYell(message))
                 return "Calm down, I know what I'm doing!";
             if(isYell(message))
                 return "Whoa, chill out!";
+            
             return "Whatever";
         }
 
@@ -72,5 +80,17 @@ namespace Activity12{
         private static bool isYell(string message){
             return message.Any(char.IsLetter) && message.ToUpperInvariant() == message;
         }
+
+        public static void intervalTimer(){
+            tTimes = new System.Timers.Timer(5000);
+            tTimes.Elapsed += bobAwkward;
+            tTimes.AutoReset = true;
+            tTimes.Enabled = true;
+        }
+
+        public static void bobAwkward(Object source, ElapsedEventArgs e){
+            Console.WriteLine("Okay, bye!", e.SignalTime);
+        }
+
     }
 }
