75ef57167e1fefa8245666d4263d68c569db253e
Jimson Paul Serafin <paul.serafin@hcdc.edu.ph>
2022-12-01 00:11:36 +0800

    Day2 - Iteration practice

---
+++ Activity16/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Using for loop print the following output on console.
+    1
+   121
+  12321
+ 1234321
+123454321
+ 1234321
+  12321
+   121
+    1
+*/
+
+using System;
+namespace Activity16{
+    class Activity16{
+        public static void Main(string[] args){
+            Console.Write("Enter a number: ");
+            Diamond(Convert.ToInt32(Console.ReadLine() ?? "This is not a null"));
+        }
+
+        public static void Diamond(int input){
+
+            /*
+            Create a loop for row
+            Create a loop for column
+            Create a loop for space
+            */
+            int n = 0;
+            for(int row = 1; row <= input; row++){
+                for(int column = 1; column <= row; column++){
+                    for(int space = 0; space < input; space++){
+                        Console.Write(" ");
+                    }
+                    n++;
+                    if(n >= column){
+                        Console.Write(column);
+                    }
+                }
+                Console.WriteLine(" ");
+            }
+            
+        }
+    }
+}
