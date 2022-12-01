44651d0dd0fbc9c7a4a512aebeda3f5772a2d7d6
EngrJPS <paul.serafin@hcdc.edu.ph>
2022-12-01 09:14:18 +0800

    Finished Activity16

--- Activity16/Program.cs
+++ Activity16/Program.cs
@@ -26,22 +26,37 @@ namespace Activity16{
             Create a loop for column
             Create a loop for space
             */
-            int n = 0;
-            int space = input;
+            //First pyramid 
             for(int row = 1; row <= input; row++){
-                for(int column = 1; column <= row; column++){
+                //This is the loop for space
+                for(int column = 1; column <= input - row; column++){
                     Console.Write(" ");
-                    space--;
-                    if(space >= 0){
-                        Console.Write(" ");
-                    }
-                    n++;
-                    if(n >= column){
-                        Console.Write(column);
-                    }
                 }
-                
+                //This is the loop for column
+                for(int column = 1; column <= row; column++){
+                    Console.Write(column);
+                }
+                //This is the loop for number
+                for(int i = row - 1; i >= 1; i--){
+                    Console.Write(i);
+                }
                 Console.WriteLine(" ");
+            }
+            //Second pyramid
+            for(int row = input - 1; row >= 1; row--){
+                //Loop for space
+                for(int column = 1; column <= input - row; column++){
+                    Console.Write(" ");
+                }
+                //Increase the number of columns
+                for(int column = 1; column <= row; column++){
+                    Console.Write(column);
+                }
+                //Decrease the number of rows
+                for(int i = row - 1; i >= 1; i--){
+                    Console.Write(i);
+                }
+                Console.WriteLine();
             }
             
         }
