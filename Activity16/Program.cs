8fba1e6de0ce38b23ebfdfd89f0bc720004c70e8
EngrJPS <paul.serafin@hcdc.edu.ph>
2022-12-04 17:04:30 +0800

    Function added in activity 16

--- Activity16/Program.cs
+++ Activity16/Program.cs
@@ -15,9 +15,17 @@ using System;
 namespace Activity16{
     class Activity16{
         public static void Main(string[] args){
+            Activity16 act = new Activity16();
             Console.Write("Enter a number: ");
             Diamond(Convert.ToInt32(Console.ReadLine() ?? "This is not a null"));
+            Console.Write("Enter a number: ");
+            int l = Convert.ToInt32(Console.ReadLine()?? " ");
+            Console.Write("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine()?? " ");
+            act.dumbPass(l, n);
+            act.magNum(Convert.ToInt32(Console.ReadLine()??" "));
         }
+
 
         public static void Diamond(int input){
 
@@ -52,11 +60,45 @@ namespace Activity16{
                 for(int column = 1; column <= row; column++){
                     Console.Write(column);
                 }
-                //Decrease the number of rows
+                //Decrease the values of the second half of the triangle
                 for(int i = row - 1; i >= 1; i--){
                     Console.Write(i);
                 }
                 Console.WriteLine();
+            }
+            
+        }
+
+        public void dumbPass(int n, int l){
+            for(int d1 = 1; d1 <= n; d1++){
+                for(int d2 = 1; d2 <= n; d2++){
+                    for(char l1 = 'a'; l1 < 'a' + l; l1++){
+                        for(char l2 = 'a'; l2 < 'a' + l; l2++){
+                            for(int d3 = Math.Max(d1, d2) + 1; d3 <= n; d3++){
+                                Console.Write($"{d1}{d2}{l1}{l2}{d3}" + " ");
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
+        }
+
+        public void magNum(int input){
+            for(int d1 = 0; d1 <= 9; d1++){
+                for(int d2 = 0; d2 <= 9; d2++){
+                    for(int d3 = 0; d3 <= 9; d3++){
+                        for(int d4 = 0; d4 <= 9; d4++){
+                            for(int d5 = 0; d5 <=9; d5++){
+                                for(int d6 = 0; d6 <= 9; d6++){
+                                    if(d1 * d2 * d3 * d4 * d5 * d6 == input){
+                                        Console.WriteLine($"{d1} {d2} {d3} {d4} {d5} {d6}");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
             
         }
